a62b6c5a1242d9258e839ba365ee4482ded5bf0b
Xincheng Chi <xincheng.chi@stonybrook.edu>
2022-03-26 13:39:15 -0400

    backend

---
+++ Backend/controllers/ezmaker-controller.js
@@ -0,0 +1,213 @@
+const Top5List = require('../models/top5list-model');
+
+createTop5List = (req, res) => {
+    const body = req.body;
+    if (!body) {
+        return res.status(400).json({
+            success: false,
+            error: 'You must provide a Top 5 List',
+        })
+    }
+
+    const top5List = new Top5List(body);
+    console.log("creating top5List: " + JSON.stringify(top5List));
+    if (!top5List) {
+        return res.status(400).json({ success: false, error: err })
+    }
+
+    top5List
+        .save()
+        .then(() => {
+            return res.status(201).json({
+                success: true,
+                top5List: top5List,
+                message: 'Top 5 List Created!'
+            })
+        })
+        .catch(error => {
+            return res.status(400).json({
+                error,
+                message: 'Top 5 List Not Created!'
+            })
+        })
+}
+
+updateTop5List = async (req, res) => {
+    const body = req.body
+    console.log("updateTop5List: " + JSON.stringify(body));
+    if (!body) {
+        return res.status(400).json({
+            success: false,
+            error: 'You must provide a body to update',
+        })
+    }
+
+    Top5List.findOne({ _id: req.params.id }, (err, top5List) => {
+        console.log("top5List found: " + JSON.stringify(top5List));
+        if (err) {
+            return res.status(404).json({
+                err,
+                message: 'Top 5 List not found!',
+            })
+        }
+
+        top5List.name = body.name
+        top5List.items = body.items
+        top5List.ownerEmail = body.ownerEmail
+        top5List.comments = body.comments
+        top5List.Author = body.Author
+        top5List.like = body.like
+        top5List.dislike = body.dislike
+        top5List.view = body.view
+        top5List.publish = body.publish
+        top5List.createdate = body.createdate
+        top5List.viewing = body.viewing
+        top5List.publishdate = body.publishdate
+        top5List.isCommunityList = body.isCommunityList
+        top5List.updateDate = body.updateDate
+        top5List.commentItems = body.commentItems
+
+        top5List
+            .save()
+            .then(() => {
+                console.log("SUCCESS!!!");
+                return res.status(200).json({
+                    success: true,
+                    id: top5List._id,
+                    message: 'Top 5 List updated!',
+                })
+            })
+            .catch(error => {
+                console.log("FAILURE: " + JSON.stringify(error));
+                return res.status(404).json({
+                    error,
+                    message: 'Top 5 List not updated!',
+                })
+            })
+    })
+}
+
+deleteTop5List = async (req, res) => {
+    Top5List.findById({ _id: req.params.id }, (err, top5List) => {
+        if (err) {
+            return res.status(404).json({
+                err,
+                message: 'Top 5 List not found!',
+            })
+        }
+        Top5List.findOneAndDelete({ _id: req.params.id }, () => {
+            return res.status(200).json({ success: true, data: top5List })
+        }).catch(err => console.log(err))
+    })
+}
+
+getTop5ListById = async (req, res) => {
+    await Top5List.findById({ _id: req.params.id }, (err, list) => {
+        if (err) {
+            return res.status(400).json({ success: false, error: err });
+        }
+        return res.status(200).json({ success: true, top5List: list })
+    }).catch(err => console.log(err))
+}
+getTop5Lists = async (req, res) => {
+    await Top5List.find({}, (err, top5Lists) => {
+        if (err) {
+            return res.status(400).json({ success: false, error: err })
+        }
+        if (!top5Lists.length) {
+            return res
+                .status(404)
+                .json({ success: false, error: `Top 5 Lists not found` })
+        }
+        return res.status(200).json({ success: true, data: top5Lists })
+    }).catch(err => console.log(err))
+}
+getTop5ListPairs = async (req, res) => {
+    await Top5List.find({ }, (err, top5Lists) => {
+        if (err) {
+            return res.status(400).json({ success: false, error: err })
+        }
+        if (!top5Lists) {
+            console.log("!top5Lists.length");
+            return res
+                .status(404)
+                .json({ success: false, error: 'Top 5 Lists not found' })
+        }
+        else {
+            // PUT ALL THE LISTS INTO ID, NAME PAIRS
+            let pairs = [];
+            for (let key in top5Lists) {
+                let list = top5Lists[key];
+                let pair = {
+                    _id: list._id,
+                    name: list.name,
+                    ownerEmail: list.ownerEmail,
+                    items: list.items,
+                    comments: list.comments,
+                    Author: list.Author,
+                    like: list.like,
+                    dislike: list.dislike,
+                    view: list.view,
+                    publish: list.publish,
+                    createdate: list.createdate,
+                    publishdate: list.publishdate,
+                    isCommunityList: list.isCommunityList,
+                    updateDate: list.updateDate,
+                    commentItems: list.commentItems
+                };
+                pairs.push(pair);
+            }
+            return res.status(200).json({ success: true, idNamePairs: pairs })
+        }
+    }).catch(err => console.log(err))
+}
+
+getCommunityList = async (req, res) => {
+    await Top5List.find({ }, (err, top5Lists) => {
+        if (err) {
+            return res.status(400).json({ success: false, error: err })
+        }
+        if (!top5Lists) {
+            console.log("!top5Lists.length");
+            return res
+                .status(404)
+                .json({ success: false, error: 'Top 5 Lists not found' })
+        }
+        else {
+            // PUT ALL THE LISTS INTO ID, NAME PAIRS
+            let pairs = [];
+            for (let key in top5Lists) {
+                let list = top5Lists[key];
+                if(list.isCommunityList){
+                    let pair = {
+                        _id: list._id,
+                        name: list.name,
+                        items: list.items,
+                        comments: list.comments,
+                        like: list.like,
+                        dislike: list.dislike,
+                        view: list.view,
+                        publish: list.publish,
+                        createdate: list.createdate,
+                        publishdate: list.publishdate,
+                        isCommunityList: list.isCommunityList,
+                        updateDate: list.updateDate,
+                        commentItems: list.commentItems
+                    };
+                    pairs.push(pair);
+                }
+            }
+            return res.status(200).json({ success: true, idNamePairs: pairs })
+        }
+    }).catch(err => console.log(err))
+}
+
+module.exports = {
+    createTop5List,
+    updateTop5List,
+    deleteTop5List,
+    getTop5Lists,
+    getTop5ListPairs,
+    getTop5ListById,
+    getCommunityList
+}
---
+++ Backend/controllers/user-controller.js
@@ -0,0 +1,210 @@
+const auth = require('../auth')
+const User = require('../models/user-model')
+const bcrypt = require('bcryptjs')
+
+getLoggedIn = async (req, res) => {
+    auth.verify(req, res, async function () {
+        const loggedInUser = await User.findOne({ _id: req.userId });
+        return res.status(200).json({
+            loggedIn: true,
+            user: {
+                firstName: loggedInUser.firstName,
+                lastName: loggedInUser.lastName,
+                email: loggedInUser.email,
+                userName: loggedInUser.userName,
+                _id: loggedInUser._id,
+                likedList: loggedInUser.likedList,
+                dislikedList: loggedInUser.dislikedList
+            }
+        }).send();
+    })
+}
+loginUser = async(req, res) => {
+    try {
+        const { email, password } = req.body;
+        if (!email || !password) {
+            return res.status(201)
+                        .json({ errorMessage: "Please enter all required fields." });
+        }
+        if (password.length < 8) {
+            return res.status(201)
+                    .json({
+                    errorMessage: "Please enter a password of at least 8 characters."
+                });
+        }
+        const exist1 = await User.findOne({ email: email });
+        const exist2 = await User.findOne({ userName: email });
+        if (!exist1 && !exist2) {
+            return res
+                .status(201)
+                .json({
+                    success: false,
+                    errorMessage: "An account with this email address or user name does not exists."
+                });
+        }
+        let existingUser = exist1;
+        if(!exist1){
+            existingUser = exist2;
+        }
+        console.log(existingUser)
+        if(existingUser.password != password){
+            return res
+                .status(201).json({
+                    success: false,
+                    errorMessage: "Invalid password."
+                });
+        }
+
+        // LOGIN THE USER
+        const token = auth.signToken(existingUser);
+        await res.cookie("token", token, {
+            httpOnly: true,
+            secure: true,
+            sameSite: "none"
+        }).status(200).json({
+            success: true,
+            user: {
+                firstName: existingUser.firstName,
+                lastName: existingUser.lastName,
+                email: existingUser.email,
+                userName: existingUser.userName,
+                _id: existingUser._id,
+                likedList: existingUser.likedList,
+                dislikedList: existingUser.dislikedList
+            }
+        }).send();
+    } catch (err) {
+        console.error(err);
+        res.status(500).send();
+    }
+}
+registerUser = async (req, res) => {
+    try {
+        const { firstName, lastName, email, password, passwordVerify,userName } = req.body;
+        if (!firstName || !lastName || !email || !password || !passwordVerify || !userName) {
+            return res.status(201)
+                        .json({ errorMessage: "Please enter all required fields." });
+        }
+        if (password.length < 8) {
+            return res.status(201)
+                    .json({
+                    errorMessage: "Please enter a password of at least 8 characters."
+                });
+        }
+        if (password !== passwordVerify) {
+            return res
+                .status(201)
+                .json({
+                    errorMessage: "Please enter the same password twice."
+                })
+        }
+        const existingUser = await User.findOne({ email: email });
+        if (existingUser) {
+            return res
+                .status(201)
+                .json({
+                    success: false,
+                    errorMessage: "An account with this email address already exists."
+                })
+        }
+        const exists = await User.findOne({ userName: userName });
+        if (exists) {
+            return res
+                .status(201)
+                .json({
+                    success: false,
+                    errorMessage: "An account with this user name already exists."
+                })
+        }
+        const saltRounds = 10;
+        const salt = await bcrypt.genSalt(saltRounds);
+        const passwordHash = await bcrypt.hash(password, salt);
+
+        const newUser = new User({
+            firstName, lastName, email, passwordHash, password, userName
+        });
+        const savedUser = await newUser.save();
+
+        // LOGIN THE USER
+        const token = auth.signToken(savedUser);
+
+        await res.cookie("token", token, {
+            httpOnly: true,
+            secure: true,
+            sameSite: "none"
+        }).status(200).json({
+            success: true,
+            user: {
+                firstName: savedUser.firstName,
+                lastName: savedUser.lastName,
+                email: savedUser.email,
+                userName: savedUser.userName,
+                _id: savedUser._id,
+                likedList: savedUser.likedList,
+                dislikedList: savedUser.dislikedList
+            }
+        }).send();
+    } catch (err) {
+        console.error(err);
+        res.status(500).send();
+    }
+}
+
+getUserById = async (req, res) => {
+    await User.findById({ _id: req.params.id }, (err, user) => {
+        if (err) {
+            return res.status(400).json({ success: false, error: err });
+        }
+        return res.status(200).json({ success: true, user: user })
+    }).catch(err => console.log(err))
+}
+
+updateUser = async (req, res) => {
+    const body = req.body
+    console.log("updateUser: " + JSON.stringify(body));
+    if (!body) {
+        return res.status(400).json({
+            success: false,
+            error: 'You must provide a body to update',
+        })
+    }
+
+    User.findOne({ _id: req.params.id }, (err, user) => {
+        console.log("User found: " + JSON.stringify(user));
+        if (err) {
+            return res.status(404).json({
+                err,
+                message: 'User not found!',
+            })
+        }
+
+        user.likedList = body.likedList
+        user.dislikedList = body.dislikedList
+
+        user
+            .save()
+            .then(() => {
+                console.log("SUCCESS!!!");
+                return res.status(200).json({
+                    success: true,
+                    id: user._id,
+                    message: 'User updated!',
+                })
+            })
+            .catch(error => {
+                console.log("FAILURE: " + JSON.stringify(error));
+                return res.status(404).json({
+                    error,
+                    message: 'User not updated!',
+                })
+            })
+    })
+}
+
+module.exports = {
+    getLoggedIn,
+    registerUser,
+    loginUser,
+    getUserById,
+    updateUser
+}
