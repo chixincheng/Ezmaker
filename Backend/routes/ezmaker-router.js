7394d318016bf65ef4c7af07d37def59fcf88827
Xincheng Chi <xincheng.chi@stonybrook.edu>
2022-03-26 17:57:03 -0400

    update

--- Backend/routes/ezmaker-router.js
+++ Backend/routes/ezmaker-router.js
@@ -11,7 +11,7 @@ router.get('/communityComics', auth.verify, EasyMakerController.getCommunityComi
 router.get('/communityStories', auth.verify, EasyMakerController.getCommunityStories)
 
 // create a new comic object in Comic table
-router.get('/createComic',  EasyMakerController.createComic)
+router.post('/createComic', auth.verify, EasyMakerController.createComic)
 
 // modify a partifuclar comic object in Comic table by id
 router.put('/editComic', auth.verify, EasyMakerController.editComic)
