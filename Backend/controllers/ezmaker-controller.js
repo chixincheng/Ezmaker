be7287cb36e2dd8e37da979f110af5c6980e3609
Xincheng Chi <xincheng.chi@stonybrook.edu>
2022-03-26 14:59:45 -0400

    controller update

--- Backend/controllers/ezmaker-controller.js
+++ Backend/controllers/ezmaker-controller.js
@@ -1,5 +1,5 @@
 const ezmaker = require('../models/ezmaker-model');
-
+/*
 createTop5List = (req, res) => {
     const body = req.body;
     if (!body) {
@@ -210,4 +210,360 @@ module.exports = {
     getTop5ListPairs,
     getTop5ListById,
     getCommunityList
+}*/
+
+// get community comics
+getCommunityComics = async (req, res) => {
+    await Top5List.find({ }, (err, top5Lists) => {
+        if (err) {
+            return res.status(400).json({ success: false, error: err })
+        }
+        if (!top5Lists) {
+            console.log("!top5Lists.length");
+            return res
+                .status(404)
+                .json({ success: false, error: 'Top 5 Lists not found' })
+        }
+        else {
+            // PUT ALL THE LISTS INTO ID, NAME PAIRS
+            let pairs = [];
+            for (let key in top5Lists) {
+                let list = top5Lists[key];
+                if(list.isCommunityList){
+                    let pair = {
+                        _id: list._id,
+                        name: list.name,
+                        items: list.items,
+                        comments: list.comments,
+                        like: list.like,
+                        dislike: list.dislike,
+                        view: list.view,
+                        publish: list.publish,
+                        createdate: list.createdate,
+                        publishdate: list.publishdate,
+                        isCommunityList: list.isCommunityList,
+                        updateDate: list.updateDate,
+                        commentItems: list.commentItems
+                    };
+                    pairs.push(pair);
+                }
+            }
+            return res.status(200).json({ success: true, idNamePairs: pairs })
+        }
+    }).catch(err => console.log(err))
+}
+
+// get community stories
+getCommunityStories = async (req, res) => {
+    await Top5List.find({ }, (err, top5Lists) => {
+        if (err) {
+            return res.status(400).json({ success: false, error: err })
+        }
+        if (!top5Lists) {
+            console.log("!top5Lists.length");
+            return res
+                .status(404)
+                .json({ success: false, error: 'Top 5 Lists not found' })
+        }
+        else {
+            // PUT ALL THE LISTS INTO ID, NAME PAIRS
+            let pairs = [];
+            for (let key in top5Lists) {
+                let list = top5Lists[key];
+                if(list.isCommunityList){
+                    let pair = {
+                        _id: list._id,
+                        name: list.name,
+                        items: list.items,
+                        comments: list.comments,
+                        like: list.like,
+                        dislike: list.dislike,
+                        view: list.view,
+                        publish: list.publish,
+                        createdate: list.createdate,
+                        publishdate: list.publishdate,
+                        isCommunityList: list.isCommunityList,
+                        updateDate: list.updateDate,
+                        commentItems: list.commentItems
+                    };
+                    pairs.push(pair);
+                }
+            }
+            return res.status(200).json({ success: true, idNamePairs: pairs })
+        }
+    }).catch(err => console.log(err))
+}
+
+//create comics
+createComic = (req, res) => {
+    const body = req.body;
+    if (!body) {
+        return res.status(400).json({
+            success: false,
+            error: 'You must provide a Top 5 List',
+        })
+    }
+
+    const top5List = new Top5List(body);
+    console.log("creating top5List: " + JSON.stringify(top5List));
+    if (!top5List) {
+        return res.status(400).json({ success: false, error: err })
+    }
+
+    top5List
+        .save()
+        .then(() => {
+            return res.status(201).json({
+                success: true,
+                top5List: top5List,
+                message: 'Top 5 List Created!'
+            })
+        })
+        .catch(error => {
+            return res.status(400).json({
+                error,
+                message: 'Top 5 List Not Created!'
+            })
+        })
+}
+
+//edit comics
+editComic = async (req, res) => {
+    const body = req.body
+    console.log("updateTop5List: " + JSON.stringify(body));
+    if (!body) {
+        return res.status(400).json({
+            success: false,
+            error: 'You must provide a body to update',
+        })
+    }
+
+    Top5List.findOne({ _id: req.params.id }, (err, top5List) => {
+        console.log("top5List found: " + JSON.stringify(top5List));
+        if (err) {
+            return res.status(404).json({
+                err,
+                message: 'Top 5 List not found!',
+            })
+        }
+
+        top5List.name = body.name
+        top5List.items = body.items
+        top5List.ownerEmail = body.ownerEmail
+        top5List.comments = body.comments
+        top5List.Author = body.Author
+        top5List.like = body.like
+        top5List.dislike = body.dislike
+        top5List.view = body.view
+        top5List.publish = body.publish
+        top5List.createdate = body.createdate
+        top5List.viewing = body.viewing
+        top5List.publishdate = body.publishdate
+        top5List.isCommunityList = body.isCommunityList
+        top5List.updateDate = body.updateDate
+        top5List.commentItems = body.commentItems
+
+        top5List
+            .save()
+            .then(() => {
+                console.log("SUCCESS!!!");
+                return res.status(200).json({
+                    success: true,
+                    id: top5List._id,
+                    message: 'Top 5 List updated!',
+                })
+            })
+            .catch(error => {
+                console.log("FAILURE: " + JSON.stringify(error));
+                return res.status(404).json({
+                    error,
+                    message: 'Top 5 List not updated!',
+                })
+            })
+    })
+}
+
+//delete the comic by id 
+deleteComic = async (req, res) => {
+    Top5List.findById({ _id: req.params.id }, (err, top5List) => {
+        if (err) {
+            return res.status(404).json({
+                err,
+                message: 'Top 5 List not found!',
+            })
+        }
+        Top5List.findOneAndDelete({ _id: req.params.id }, () => {
+            return res.status(200).json({ success: true, data: top5List })
+        }).catch(err => console.log(err))
+    })
+}
+
+
+//create stories
+createStory = (req, res) => {
+    const body = req.body;
+    if (!body) {
+        return res.status(400).json({
+            success: false,
+            error: 'You must provide a Top 5 List',
+        })
+    }
+
+    const top5List = new Top5List(body);
+    console.log("creating top5List: " + JSON.stringify(top5List));
+    if (!top5List) {
+        return res.status(400).json({ success: false, error: err })
+    }
+
+    top5List
+        .save()
+        .then(() => {
+            return res.status(201).json({
+                success: true,
+                top5List: top5List,
+                message: 'Top 5 List Created!'
+            })
+        })
+        .catch(error => {
+            return res.status(400).json({
+                error,
+                message: 'Top 5 List Not Created!'
+            })
+        })
+}
+
+//edit stories
+editStory = async (req, res) => {
+    const body = req.body
+    console.log("updateTop5List: " + JSON.stringify(body));
+    if (!body) {
+        return res.status(400).json({
+            success: false,
+            error: 'You must provide a body to update',
+        })
+    }
+
+    Top5List.findOne({ _id: req.params.id }, (err, top5List) => {
+        console.log("top5List found: " + JSON.stringify(top5List));
+        if (err) {
+            return res.status(404).json({
+                err,
+                message: 'Top 5 List not found!',
+            })
+        }
+
+        top5List.name = body.name
+        top5List.items = body.items
+        top5List.ownerEmail = body.ownerEmail
+        top5List.comments = body.comments
+        top5List.Author = body.Author
+        top5List.like = body.like
+        top5List.dislike = body.dislike
+        top5List.view = body.view
+        top5List.publish = body.publish
+        top5List.createdate = body.createdate
+        top5List.viewing = body.viewing
+        top5List.publishdate = body.publishdate
+        top5List.isCommunityList = body.isCommunityList
+        top5List.updateDate = body.updateDate
+        top5List.commentItems = body.commentItems
+
+        top5List
+            .save()
+            .then(() => {
+                console.log("SUCCESS!!!");
+                return res.status(200).json({
+                    success: true,
+                    id: top5List._id,
+                    message: 'Top 5 List updated!',
+                })
+            })
+            .catch(error => {
+                console.log("FAILURE: " + JSON.stringify(error));
+                return res.status(404).json({
+                    error,
+                    message: 'Top 5 List not updated!',
+                })
+            })
+    })
+}
+
+//delete the story by id 
+deleteStory = async (req, res) => {
+    Top5List.findById({ _id: req.params.id }, (err, top5List) => {
+        if (err) {
+            return res.status(404).json({
+                err,
+                message: 'Top 5 List not found!',
+            })
+        }
+        Top5List.findOneAndDelete({ _id: req.params.id }, () => {
+            return res.status(200).json({ success: true, data: top5List })
+        }).catch(err => console.log(err))
+    })
+}
+
+// get comic by id
+getComicByID = async (req, res) => {
+    await Top5List.findById({ _id: req.params.id }, (err, list) => {
+        if (err) {
+            return res.status(400).json({ success: false, error: err });
+        }
+        return res.status(200).json({ success: true, top5List: list })
+    }).catch(err => console.log(err))
+}
+
+// get story by id
+getStoryByID = async (req, res) => {
+    await Top5List.findById({ _id: req.params.id }, (err, list) => {
+        if (err) {
+            return res.status(400).json({ success: false, error: err });
+        }
+        return res.status(200).json({ success: true, top5List: list })
+    }).catch(err => console.log(err))
+}
+
+// get all user comics
+getAllUserComics = async (req, res) => {
+    await Top5List.find({}, (err, top5Lists) => {
+        if (err) {
+            return res.status(400).json({ success: false, error: err })
+        }
+        if (!top5Lists.length) {
+            return res
+                .status(404)
+                .json({ success: false, error: `Top 5 Lists not found` })
+        }
+        return res.status(200).json({ success: true, data: top5Lists })
+    }).catch(err => console.log(err))
+}
+
+// get all user stories
+getAllUserStories = async (req, res) => {
+    await Top5List.find({}, (err, top5Lists) => {
+        if (err) {
+            return res.status(400).json({ success: false, error: err })
+        }
+        if (!top5Lists.length) {
+            return res
+                .status(404)
+                .json({ success: false, error: `Top 5 Lists not found` })
+        }
+        return res.status(200).json({ success: true, data: top5Lists })
+    }).catch(err => console.log(err))
+}
+
+module.exports = {
+    getCommunityComics,
+    getCommunityStories,
+    createComic,
+    editComic,
+    deleteComic,
+    createStory,
+    editStory,
+    deleteStory,
+    getComicByID,
+    getStoryByID,
+    getAllUserComics,
+    getAllUserStories,
 }
