50ed16fff71c55416e876c61c1e8eebaa09444c0
GankOwen <68822609+GankOwen@users.noreply.github.com>
2022-04-05 21:33:51 -0400

    icon update

---
+++ Frontend/my-app/src/Images/index.js
@@ -0,0 +1,30 @@
+import check from '../Images/check.png'
+import clock from '../Images/clock.png'
+import comicApp from '../Images/comicApp.png'
+import communityIcon from '../Images/communityIcon.png'
+import cover1 from '../Images/cover1.png'
+import easyToUse from '../Images/easyToUSe.png'
+import editInfo from '../Images/editInfo.png'
+import icon from '../Images/icon.png'
+import landingPageBackground from '../Images/landingPageBackground.png'
+import loginButton from '../Images/loginButton.png'
+import makingForEveryone from '../Images/makingForEveryone.png'
+import playlist from '../Images/playlist.png'
+import storyApp from '../Images/storyApp.png'
+import verify from '../Images/verify.png'
+import weAllAreListening from '../Images/weAllAreListening.png'
+import discussion from '../Images/discussion.png'
+import download from '../Images/download.png'
+import notVerify from '../Images/notVerify.png'
+import addFav from '../Images/addFav.png'
+import removeFav from '../Images/removeFav.png'
+import like from '../Images/like.png'
+import commentSend from '../Images/commentSend.png'
+
+const images = {
+    check, clock, comicApp, communityIcon, cover1, easyToUse,
+    editInfo, icon, landingPageBackground, loginButton, makingForEveryone,
+    playlist, storyApp, verify, verify, weAllAreListening, discussion
+    , download, notVerify, addFav, removeFav, like, commentSend
+}
+export default images
--- Frontend/my-app/src/Pages/DashboardPage.js
+++ Frontend/my-app/src/Pages/DashboardPage.js
@@ -1,17 +1,18 @@
 import { Fragment } from "react";
 import { useNavigate } from "react-router-dom";
 
-const DashboardPage = ()=>{
+
+const DashboardPage = () => {
     const navigate = useNavigate();
 
-    return(<div style={{display:"flex", justifyContent:"space-evenly", alignItems:"center", width:"100vw", height:"100vh"}}>
-        <button style={{width:"500px",height:"500px"}} onClick={()=>{ navigate("/dashboard/comic/home"); }}>
+    return (<div style={{ display: "flex", justifyContent: "space-evenly", alignItems: "center", width: "100vw", height: "100vh" }}>
+        <button style={{ width: "500px", height: "500px", background: "" }} onClick={() => { navigate("/dashboard/comic/home"); }}>
             Comic App
         </button>
-        <button style={{width:"500px",height:"500px"}}  onClick={()=>{ navigate("/dashboard/story/home"); }}>
+        <button style={{ width: "500px", height: "500px" }} onClick={() => { navigate("/dashboard/story/home"); }}>
             Story App
         </button>
     </div>);
 };
 
-export default  DashboardPage;
+export default DashboardPage;
