c3fa70348628270cbd1a7cd48895076cafb2a5fb
GankOwen <68822609+GankOwen@users.noreply.github.com>
2022-04-05 23:14:56 -0400

    icons updated 3

--- Frontend/my-app/src/Images/index.js
+++ Frontend/my-app/src/Images/index.js
@@ -20,11 +20,44 @@ import addFav from '../Images/addFav.png'
 import removeFav from '../Images/removeFav.png'
 import like from '../Images/like.png'
 import commentSend from '../Images/commentSend.png'
+import unlike from '../Images/unlike.png'
+import categoryAdd from '../Images/categoryAdd.png'
+import comicsDetailSample from '../Images/comicsDetailSample.png'
+import comicsDetailSample2 from '../Images/comicsDetailSample2.png'
+import categoryTitle from '../Images/categoryTitle.png'
+import deleteIcon from '../Images/delete.png'
+import comicsDownload from '../Images/comicsDownload.png'
+import dribbble from '../Images/dribbble.png'
+import eraser from '../Images/eraser.png'
+import facebook from '../Images/facebook.png'
+import colorFill from '../Images/colorFill.png'
+import instagram from '../Images/instagram.png'
+import textOrTextSize from '../Images/textOrTextSize.png'
+import removePage from '../Images/removePage.png'
+import pen from '../Images/pen.png'
+import addPage from '../Images/addPage.png'
+import publish from '../Images/publish.png'
+import save from '../Images/save.png'
+import saveSuccessed from '../Images/saveSuccessed.png'
+import upload from '../Images/upload.png'
+import bold from '../Images/bold.png'
+import colorChange from '../Images/colorChange.png'
+import highlighter from '../Images/highlighter.png'
+import italic from '../Images/italic.png'
+import underline from '../Images/underline.png'
+
+
+
+
 
 const images = {
     check, clock, comicApp, communityIcon, cover1, easyToUse,
     editInfo, icon, landingPageBackground, loginButton, makingForEveryone,
     playlist, storyApp, verify, verify, weAllAreListening, discussion
-    , download, notVerify, addFav, removeFav, like, commentSend
+    , download, notVerify, addFav, removeFav, like, commentSend,
+    unlike, categoryAdd, comicsDetailSample, comicsDetailSample2,
+    categoryTitle, deleteIcon, comicsDownload, dribbble, eraser, facebook,
+    colorFill, instagram, textOrTextSize, removePage, pen, addPage, publish,
+    save, saveSuccessed, upload, bold, colorChange, highlighter, italic, underline
 }
 export default images
