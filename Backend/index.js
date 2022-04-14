a2ab2c5d9746e91facc39ccac8a86fc1b82b603c
GankOwen <68822609+GankOwen@users.noreply.github.com>
2022-04-13 23:32:22 -0400

    backend change cors .url

--- Backend/index.js
+++ Backend/index.js
@@ -12,7 +12,7 @@ const app = express()
 // SETUP THE MIDDLEWARE
 app.use(express.urlencoded({ extended: true }))
 app.use(cors({
-    origin: ["http://localhost:3000"],
+    origin: ["https://ezmaker-bfcd6.web.app"],
     credentials: true
 }))
 app.use(express.json())
